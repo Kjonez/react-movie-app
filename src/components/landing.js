cd8c73ba3c389222fe75e0547e7089019e347344
kieran jones <Kieran Jones>
2017-09-03 23:32:04 -0700

    youtube api

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,21 +1,27 @@
-import React, {Component} from 'react';
-import { Text, TextInput, View } from 'react-native'; //View is primitve element to position/wrap/style
-import {NativeRouter, Route, Link} from 'react-router-native';
-import SearchBar from './searchbar.js';
+import React from "react";
+import { Text, TextInput, View } from "react-native"; //View is primitve element to position/wrap/style
+import SearchBar from "./searchbar.js";
 import Title from './title.js';
-import Results from './results.js';
 
-class Landing extends Component {
+const Landing = props => {
+  return (
+  	<View>
+  		<View>
+  			<Title />	
+  		</View>
+  		<View
+	      style={{
+	        flexDirection: "row",
+	        height: 700,
+	        padding: 20,
+	        alignSelf: "stretch"
+	      }}>
+	      <SearchBar />
+	    </View>
+  	</View>   
+  );
+};
 
-	render(){
-		return (
-		<View> 
-			<Title />
-			<SearchBar />
-			<Results />
-		</View>	
-		);
-	}
-}	
+//camel case instead of dashes for object css
 
 export default Landing;
